df88e3e5da3e0ac8829cbd8fbe35f9940063a193
unknown <ananychitranshi0701@gmail.com>
2020-09-18 03:08:47 +0530

    Bugs fixed - Program doesn't crashes in case of no path

--- scripts.js
+++ scripts.js
@@ -1,16 +1,32 @@
 // Initial setup of rows and columns
+let origin_selected=false;
 let flag=0;
-let start_node_row=Math.floor(Math.random() * 31);  
-let start_node_col=Math.floor(Math.random() * 61);
-let end_node_row=Math.floor(Math.random() * 31);
-let end_node_col=Math.floor(Math.random() * 31);
+let table_rows=25
+let table_columns=55
+let start_node_row,start_node_col,end_node_col,end_node_row,src_define,dest_define,src_id,dest_id;
+start_node_row=Math.floor(Math.random() * (table_rows));  
+start_node_col=Math.floor(Math.random() * (table_columns));
+end_node_row=Math.floor(Math.random() * (table_rows));
+end_node_col=Math.floor(Math.random() * (table_columns));
 
-let src_define=start_node_row+'+'+start_node_col;
-let dest_define=end_node_row+'+'+end_node_col;
-let src_id='#tr'+start_node_row+' .td'+start_node_col;
-let dest_id='#tr'+end_node_row+' .td'+end_node_col;
+src_define=start_node_row+'+'+start_node_col;
+dest_define=end_node_row+'+'+end_node_col;
+src_id='#tr'+start_node_row+' .td'+start_node_col;
+dest_id='#tr'+end_node_row+' .td'+end_node_col;
 
-    
+const initialize_variables=(r,c)=>{
+
+
+start_node_col=Math.floor(Math.random() * (table_columns));
+end_node_row=Math.floor(Math.random() * (table_rows));
+end_node_col=Math.floor(Math.random() * (table_columns));
+
+src_define=start_node_row+'+'+start_node_col;
+dest_define=end_node_row+'+'+end_node_col;
+src_id='#tr'+start_node_row+' .td'+start_node_col;
+dest_id='#tr'+end_node_row+' .td'+end_node_col;
+
+}   
 const seg=(s)=>{
   let op=''
   for(let i=0;i<s.length;i++)
@@ -38,21 +54,18 @@ const extractcol= (s)=>
     return output;
 }
 
-const rseg=(s)=>{
+const rseg=(s)=>
+{
   let x=s.indexOf('+');
   let op=s.substring(x+1);
-
   return rseg;
 }
 let table=document.getElementById('tbl');
 console.log(table);
-for(let i=0;i<31;i++)
+for(let i=0;i<table_rows;i++)
 {
   let tre=document.createElement('tr');
   let idx='tr';
-  // if(i<=9)
-  //   idx+='0'+i;
-  // else
   idx+=i;
   tre.setAttribute('id',idx);
   table.appendChild(tre);
@@ -66,10 +79,10 @@ let mouseclicked=false;
 let arr=[];
 let src_ind;
 let dest_ind;
-for(let i=0;i<31;i++)
+for(let i=0;i<table_rows;i++)
       {
         let a2=[];
-        for(let j=0;j<61;j++)
+        for(let j=0;j<table_columns;j++)
         {
           a2.push(1);
         }
@@ -77,12 +90,26 @@ for(let i=0;i<31;i++)
       }
 const setup=()=>
 {   
-    for (let i=0;i<31;i++)
+
+
+    start_node_row=Math.floor(Math.random() * (table_rows));  
+    start_node_col=Math.floor(Math.random() * (table_columns));
+    end_node_row=Math.floor(Math.random() * (table_rows));
+    end_node_col=Math.floor(Math.random() * (table_columns));
+   
+    src_define=start_node_row+'+'+start_node_col;
+    dest_define=end_node_row+'+'+end_node_col;
+    src_id='#tr'+start_node_row+' .td'+start_node_col;
+    dest_id='#tr'+end_node_row+' .td'+end_node_col;
+
+
+
+    for (let i=0;i<table_rows;i++)
       {
         let row;
         row="tr"+i;
         let t=document.getElementById(row);
-        for(let j=0;j<61;j++)
+        for(let j=0;j<table_columns;j++)
           {
             let p=document.createElement('td');
             let col="td"+j;
@@ -97,18 +124,20 @@ const setup=()=>
         col=extractcol(col);
         let row_num=Number(row.substring(2));
         let col_num=Number(col.substring(2));
-        console.log('---->',col);
-        console.log('New ',extractcol(col));
         let selector='#'+row+' '+'.'+col;
         console.log('-->',selector)
         console.log('---')
-
+        if(selector==src_id || selector==dest_id)
+            {
+                origin_selected='true';
+                return ;
+            }
         if(!document.querySelector(selector).classList.contains('tblcell'))
         {
             document.querySelector(selector).classList.add('tblcell');
 
      
-        arr[row_num][col_num]=1000000;
+        arr[row_num][col_num]=Infinity ;
         }
         else
         {
@@ -121,15 +150,17 @@ const setup=()=>
       });
       document.getElementById('tbl').addEventListener('mousedown',()=>{
         mouseclicked=true;
-      //  console.log('Mouse Pressed')
+   
       })
 
 
     document.getElementById('tbl').addEventListener('mouseup',()=>{
       mouseclicked=false;
-    //  console.log('Mouse released')
+   
     })
+    console.log(src_id);
     document.querySelector(src_id).classList.add('source')
+    console.log(dest_id);
     document.querySelector(dest_id).classList.add('destination');
 
     document.getElementById('tbl').addEventListener('mouseover',(e)=>{
@@ -142,13 +173,18 @@ const setup=()=>
           let col_num=Number(col.substring(2));
           
           let selector='#'+row+' '+'.'+col;
-    
+          if(origin_selected==true)
+          {
+
+          }
+          if(selector==src_id || selector==dest_id)
+          return ;
           if(!document.querySelector(selector).classList.contains('tblcell'))
           {
               document.querySelector(selector).classList.add('tblcell');
   
     
-          arr[row_num][col_num]=1000000;
+          arr[row_num][col_num]=Infinity ;
           }
           else
           {
@@ -202,12 +238,12 @@ const dijkstra= (arr,src,dest,printPath)=>
   let parent={};
     parent[src]=-1;
     let dist_from_source = {};
-    for(let i=0;i<31 ;i++)
+    for(let i=0;i<table_rows ;i++)
       {
-        for(let j=0;j<61;j++)
+        for(let j=0;j<table_columns;j++)
           {
             let ind=i+'+'+j;
-          dist_from_source[ind]=1000;
+          dist_from_source[ind]=Infinity ;
           if(ind==src) 
             dist_from_source[ind]=0;
       } 
@@ -217,7 +253,7 @@ const dijkstra= (arr,src,dest,printPath)=>
 
 for(let j=1;j<=len && !visited.includes(dest);j++)
 {
-  let mini=100000;
+  let mini=Infinity;
   let ind=src;
     for(i in dist_from_source) 
     {
@@ -230,12 +266,17 @@ for(let j=1;j<=len && !visited.includes(dest);j++)
         }
        
     }
+    if(!isFinite(mini))
+        {
+           console.log('INININININININI');
+           return ; 
+        }
     let r=Number(seg(ind));
     let c=Number(ind.substring(2));
-    console.log('------->',r,c)
+   
     let str='#tr'+r+' .td'+c;
     let interval=setTimeout(()=>{
-      if((ind!=src && ind!=dest))
+      if((ind!=src && ind!=dest && mini!=Infinity))
       {
       let u= document.querySelector(str)
       console.log('-->',u.getAttribute('class'))
@@ -244,18 +285,13 @@ for(let j=1;j<=len && !visited.includes(dest);j++)
       let inter;
       if(ind==dest)
       {
-       // clearInterval(inter);
+   
        printPath(output);
         flag=1;
       }
 
       console.log(flag);
     },10*j)
-    // if(j==len || ind==dest)
-    // {
-    //   clearInterval(interval);
-    //   printPath(output);
-    // }
     visited.push(ind);
     if(r-1>=0)
     {
@@ -269,7 +305,7 @@ for(let j=1;j<=len && !visited.includes(dest);j++)
             parent[comb]=ind;
         }
     }
-    if(r+1<31)
+    if(r+1<table_rows)
     {
         let cr=r+1;
         let cc=c;
@@ -283,7 +319,7 @@ for(let j=1;j<=len && !visited.includes(dest);j++)
         
     }
 
-    if(c+1<61)
+    if(c+1<table_columns)
     {
         let cr=r;
         let cc=c+1;
@@ -317,15 +353,22 @@ let xx=dest;
 console.log(dest)
 output=[]
 console.log(parent)
-while(parent[xx]!=-1)
+let counter=0;
+while(parent[xx]!=-1 && counter<=100000)
 {
-  
+  counter+=1;
+  if(parent[xx]!=undefined)
+  {
   console.log(parent[xx]);
+ 
   output.push(parent[xx]);
   xx=parent[xx];
+  }
+  
 }
 // printPath(output);
 // return output;
+
 console.log('finished!!')
 
 }
@@ -334,11 +377,9 @@ console.log('finished!!')
 
 document.getElementById('initiate').addEventListener('click',()=>{
   alert('clicked!!')
-
-
-
- 
   
+  
+
 dijkstra(arr,src_define,dest_define,printPath);
   
 document.getElementById('clear').addEventListener('click',()=>{
@@ -347,7 +388,18 @@ document.getElementById('clear').addEventListener('click',()=>{
 });
 
 
+window.addEventListener('resize',()=>{
+    console.log(window.innerHeight);
+    console.log(window.innerWidth);
+    // let w=window.innerWidth;
+    // let h=window.innerHeight;
+    //  table_rows=Number(Math.floor(w/27));
+    //   table_columns=Number(Math.floor(h/54));
+    // console.log(table_rows,table_columns);
 
+    // setup();
+
+})
 
 
 
